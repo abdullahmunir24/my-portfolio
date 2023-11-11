a5090947a6b74ecd2fe4f95cd44ea52eb1f1dfd7
Abdullah Munir <abdullahmunir2004@gmail.com>
2023-11-11 15:26:43 -0800

    Fixed an error

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -132,10 +132,10 @@ export default function Footer() {
       </div>
       <div className="map">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d164477.36692580467!2d-119.61958508788803!3d49.89957571728325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537d8cb6e3c730b3%3A0x4ef8e53ddab4c4f7!2sKelowna%2C%20BC!5e0!3m2!1sen!2sca!4v1698862475364!5m2!1sen!2sca"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d164477.36692580467!2d-119.61958508788804!3d49.89957571728325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537d8cb6e3c730b3%3A0x4ef8e53ddab4c4f7!2sKelowna%2C%20BC!5e0!3m2!1sen!2sca!4v1699744931489!5m2!1sen!2sca"
           width="600"
           height="450"
-          style="border:0;"
+          style={{ border: "0" }}
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
